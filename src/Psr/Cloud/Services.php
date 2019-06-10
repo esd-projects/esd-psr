6fef0cd9f7a8daed60310f4049feac362dcf952e
zhangjincheng <zhangjincheng@huidr.com>
2019-06-10 14:55:46 +0800

    create

--- src/Psr/Cloud/Services.php
+++ src/Psr/Cloud/Services.php
@@ -12,8 +12,16 @@ namespace ESD\Psr\Cloud;
 interface Services
 {
     /**
+     * 获取服务的所有信息
      * @param string $service
      * @return ServiceInfoList
      */
     public function getServices(string $service): ?ServiceInfoList;
+
+    /**
+     * 获取服务的一个信息
+     * @param string $service
+     * @return ServiceInfo
+     */
+    public function getService(string $service): ServiceInfo;
 }
